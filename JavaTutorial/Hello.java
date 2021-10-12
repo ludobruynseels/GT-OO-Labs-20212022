a96bf11ffebf6638bd0c5f9222e4753ccd44cf2c
betelgeuze <ludo.bruynseels@gmail.com>
2021-10-12 22:09:25 +0200

    add java tutorial

---
+++ JavaTutorial/Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        circle1.makeVisible();
+        circle1.moveHorizontal(100);
+        circle1.changeColor("green");
+
+        Circle frontWheel = new Circle();
+        frontWheel.makeVisible();
+
+        Circle backWheel = new Circle();
+        backWheel.makeVisible();
+
+        Square body;
+        Square body2;
+        Square body3;
+    }
+
+}
